f6fea5512daefc38bffa175a20e80ceefefbf352
vasilii <v.verkin@gmail.com>
2025-12-15 23:16:57 +0300

    создает маршрутизацию страниц

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,11 +1,24 @@
+import {BrowserRouter, Route, Routes} from 'react-router-dom';
 import MainScreen from '../../pages/main-screen/main-screen';
+import LoginScreen from '../../pages/login-screen/login-screen';
+import FavoritesScreen from '../../pages/favorites-screen/favorites-screen';
+import PropertyScreen from '../../pages/property-screen/property-screen';
 
 type AppProps = {
   offersCount: number;
 }
 
 function App({ offersCount }: AppProps): JSX.Element {
-  return <MainScreen offersCount={offersCount} />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<MainScreen offersCount={offersCount} />} />
+        <Route path='/login' element={<LoginScreen />} />
+        <Route path='/favorites' element={<FavoritesScreen />} />
+        <Route path='/offer/:id' element={<PropertyScreen />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
