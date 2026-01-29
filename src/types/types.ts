b7bfc4846a7273809cfa365f27b198a5faa2700d
vasilii <v.verkin@gmail.com>
2026-01-29 22:24:09 +0300

    Создает тип и моки отзыва

--- src/types/types.ts
+++ src/types/types.ts
@@ -25,3 +25,18 @@ export type Offer = {
     city: City;
     location: Location;
 }
+
+export type User = {
+    id: number;
+    name: string;
+    avatarUrl: string;
+    isPro: boolean;
+}
+
+export type Comment = {
+    id: number;
+    comment: string;
+    date: string;
+    rating: number;
+    user: User;
+}
